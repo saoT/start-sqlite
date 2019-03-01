43aeff7045a8249299c8ea2eb996b1b29993b4cc
saraguerric <sara.guerric@evergig.com>
2019-03-01 11:27:21 +0100

    Add : post

--- back.js
+++ back.js
@@ -3,13 +3,21 @@ const fs = require('fs');
 const sqlite3 = require('sqlite3').verbose();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const dbFile = 'test.db';
 const db = new sqlite3.Database(dbFile);
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
+
+// ---------------------------------------------------------------------------------//
+// CONSTRUCTION DE LA BASE DE DONNEE
+// ---------------------------------------------------------------------------------//
+/*
 // sans db.serialize.
 // les operations sont lancées en même temps.
 // le INSERT risque d'etre executé.
@@ -28,11 +36,9 @@ db.serialize( () => {
 
 
 
-  db.run('CREATE TABLE IF NOT EXISTS animals (animal_id INTEGER PRIMARY KEY AUTOINCREMENT, animal_name TEXT UNIQUE, type TEXT UNIQUE, student_id INTEGER, FOREIGN KEY(student_id) REFERENCES students(id) )');
+  db.run('CREATE TABLE IF NOT EXISTS animals (animal_id INTEGER PRIMARY KEY AUTOINCREMENT, animal_name TEXT UNIQUE, animal_type TEXT, student_id INTEGER, FOREIGN KEY(student_id) REFERENCES students(id) )');
 
-  db.run('INSERT INTO animals (animal_name, type, student_id) VALUES (?, ?, ?)', 'Hedwige', 'chouette', 1);
-
-
+  db.run('INSERT INTO animals (animal_name, animal_type, student_id) VALUES (?, ?, ?)', 'Hedwige', 'chouette', 1);
 
   // db.all('SELECT * FROM animals NATURAL JOIN students', function (error, data) {
   //   if (!error) console.log(data);
@@ -47,27 +53,59 @@ db.serialize( () => {
 
   db.run('INSERT INTO students_to_teachers (teacher_id, student_id) VALUES (?,?)', 1, 1);
 
-
   db.all('SELECT * FROM teachers NATURAL JOIN students_to_teachers NATURAL JOIN students NATURAL JOIN animals', function (error, data) {
     if (!error) console.log(data);
     else console.log(error);
   });
-
-
-
-
-
-
-
-
-
-
 });
+*/
+// ---------------------------------------------------------------------------------//
+// CONTROLLERS -> defini des routes avec express
+// ---------------------------------------------------------------------------------//
 
+// Je cree mes URLS SELON UNE ARCHITECTURE REST
+// URL/OBJETS_que_je_cherche_en_bdd -> URL/users -> me renvoyer tout les users
+// URL/OBJET/id -> un user
+
+// l'URL - > http://localhost:3000 + '/'
+// http://localhost:3000/
 app.get('/', function (request, response) {
   db.all('SELECT * FROM products', function (error, data) {
     response.send(data);
   });
+});
+
+// 4 verbes HTTP : GET / POST / UPDATE / DELETE
+// GET : pour recuperer du serveur, POST : pour envoyer vers le serveur
+
+// Souvent quand j'ai besoin de creer dans ma base de données
+// via un INSERT : je vais utiliser une route type POST
+
+// A l'inverse quand j'ai besoin de recuperer via un select
+// Je passe par une methode type GET
+
+
+// l'URL - > http://localhost:3000 + '/users'
+// http://localhost:3000/users
+
+
+// app.get c'est le telephone portable de notre back
+// Ici on defini le numero de portable du back
+// on dit que c'est '/students' 
+// On sait que c'est un portable donc qd on le defini
+// on defini juste la fin pas le 06
+app.get('/students', function (request, response) {
+  db.all('SELECT * FROM students', function (error, data) {
+     response.send(data);
+  });
+  //response.send(data);
+});
+
+app.post('/students', function (request, response) {
+  // INSERE DES DONNES DANS MA BDD
+  console.log(request.body.student_name);
+  db.run('INSERT INTO students (student_name) VALUES (?)', request.body.student_name);
+  response.redirect('http://www.google.com');
 });
 
 app.listen(3000, function (error) {
@@ -75,8 +113,58 @@ app.listen(3000, function (error) {
 });
 
 
-product.tshirt_name
-product[category + '_name']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
